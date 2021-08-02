3c00b95e71a88507c927c459b48d90aa4716c21b
Harimedipalli <harimedipalli5@gmai.com>
2021-08-02 10:40:39 +0530

    dec syntax

--- prblm_dec.py
+++ prblm_dec.py
@@ -1,2 +1,25 @@
+def student_dec(college):
+    def inner_function(*args):
+        # print('he is from {guntur}')
+        college(args[0])
+        for i in args[1:]:
+            # print(f'{i} is from guntur')
 
-decarative syntax with example
+            print(f'\t Student name:\t {i} ')
+        return f"{args[0]} student detalis listed above"
+
+    return inner_function
+
+
+@student_dec
+def college1(name):
+    print(f'College name is {name}')
+
+
+# @student_dec
+# def college2(name):
+#     print(f'college name is {name}')
+
+
+print(college1('khit', 'sai', 'hari'))
+print(college1('bec', 'srikant', 'mahesh'))
